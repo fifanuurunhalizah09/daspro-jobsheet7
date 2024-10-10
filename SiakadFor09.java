4d39c654c968eb2f10aecab922ff66375e984ed8
fifanuurunhalizah09 <eljeaje@gmail.com>
2024-10-10 10:30:39 +0700

    modifikasi

--- SiakadFor09.java
+++ SiakadFor09.java
@@ -6,6 +6,7 @@ public class SiakadFor09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int jumlahMahasiswa = 10, jumlahMhsLulus = 0, jumlahMhsTidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan Nilai Mahasiswa ke-" + i + ": ");
@@ -17,9 +18,15 @@ public class SiakadFor09 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
-            
+            if (nilai >= 60) {
+                jumlahMhsLulus++;
+            } else {
+                jumlahMhsTidakLulus++;
+            }
         }
-        System.out.println("Nilai tertinggi: " + tertinggi);
-            System.out.println("Nilai Terendah: " + terendah);
+        System.out.println("Nilai Tertinggi: " + tertinggi);
+        System.out.println("Nilai Terendah: " + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus: " + jumlahMhsLulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus: " + jumlahMhsTidakLulus);
     }
 }
