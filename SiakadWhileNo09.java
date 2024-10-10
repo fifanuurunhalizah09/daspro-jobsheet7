c7b90257a68598c582e77eb70a1cfdd9bbefa30b
fifanuurunhalizah09 <eljeaje@gmail.com>
2024-10-10 11:08:14 +0700

    modifikasi

--- SiakadWhileNo09.java
+++ SiakadWhileNo09.java
@@ -16,22 +16,29 @@ public class SiakadWhileNo09 {
             System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid!");
             continue;
         }
+
         if (nilai > 80 && nilai <= 100) {
-            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "adalah A");
+            System.out.println("Hasil nilai mahasiswa ke- " + (i + 1) + " adalah A");
+            if (nilai > 80 && nilai <= 100) {
+                System.out.println("Bagus, pertahankan nilainya!");
+            }
+        }
+        if (nilai > 80 && nilai <= 100) {
+            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah A");
         } else if (nilai > 73 && nilai <= 80) {
-            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "adalah B+");
+            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah B+");
         } else if (nilai > 65 && nilai <= 73) {
-            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "adalah B");
+            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah B");
         } else if (nilai > 60 && nilai <= 65) {
-            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "adalah C+");
-        } else if (nilai > 50 && nilai <= 60 ) {
-            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "adalah C");
+            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah C+");
+        } else if (nilai > 50 && nilai <= 60 ) 
+            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah C");
         } else if (nilai > 39 && nilai <= 50) {
-            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "adalah D");
+            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " adalah D");
         } else {
-            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "adalah E");
+            System.out.println("Nilai Mahasiswa ke-" + (i + 1) + " 5adalah E");
         }
         i++;
     }
-    }
+    
 }
